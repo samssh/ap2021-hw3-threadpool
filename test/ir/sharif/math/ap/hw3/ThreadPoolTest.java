03bf311bb164bf26122c27101aba3d4c0b57d641
samssh <sam.sadat.shokouhi@gmail.com>
2021-04-30 20:22:43 +0430

    some cleanup in test

--- test/ir/sharif/math/ap/hw3/ThreadPoolTest.java
+++ test/ir/sharif/math/ap/hw3/ThreadPoolTest.java
@@ -11,7 +11,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ThreadPoolTest {
     private final long TIME_SAFE_MARGIN = 50;
-    private final long RUN1_SLEEP = 200, RUN2_SLEEP = 100, RUN3_SLEEP = 500, RUN4_SLEEP = 10;
+    private final long RUN1_SLEEP = 200;
+    private final long RUN2_SLEEP = 100;
+    private final long RUN3_SLEEP = 10;
     private ThreadPool threadPool;
     private Map<Object, Object> map;
     private Thread testThread;
@@ -20,7 +22,7 @@ class ThreadPoolTest {
     void setUp() {
         this.threadPool = new ThreadPool(3);
         this.map = new ConcurrentHashMap<>();
-        testThread = Thread.currentThread();
+        this.testThread = Thread.currentThread();
     }
 
     void sleep(long l) {
@@ -71,18 +73,18 @@ class ThreadPoolTest {
         long startTime = System.currentTimeMillis();
         threadPool.setThreadNumbers(3);
         for (int i = 0; i < 6; i++) {
-            threadPool.invokeLater(this::run3);
+            threadPool.invokeLater(this::run1);
         }
         threadPool.setThreadNumbers(1);
-        sleep(RUN3_SLEEP + TIME_SAFE_MARGIN);
+        sleep(RUN1_SLEEP + TIME_SAFE_MARGIN);
         long endTime = System.currentTimeMillis();
-        assertTime(startTime, endTime, RUN3_SLEEP + 2 * TIME_SAFE_MARGIN);
+        assertTime(startTime, endTime, RUN1_SLEEP + 2 * TIME_SAFE_MARGIN);
         assertEquals(3, map.size());
-        sleep(RUN3_SLEEP + TIME_SAFE_MARGIN);
+        sleep(RUN1_SLEEP + TIME_SAFE_MARGIN);
         assertEquals(4, map.size());
-        sleep(RUN3_SLEEP + TIME_SAFE_MARGIN);
+        sleep(RUN1_SLEEP);
         assertEquals(5, map.size());
-        sleep(RUN3_SLEEP + TIME_SAFE_MARGIN);
+        sleep(RUN1_SLEEP);
         assertEquals(6, map.size());
     }
 
@@ -91,11 +93,11 @@ class ThreadPoolTest {
         long startTime = System.currentTimeMillis();
         threadPool.setThreadNumbers(10);
         for (int i = 0; i < 10; i++) {
-            threadPool.invokeLater(this::run4);
+            threadPool.invokeLater(this::run3);
         }
-        sleep(RUN1_SLEEP + RUN4_SLEEP + TIME_SAFE_MARGIN);
+        sleep(RUN1_SLEEP + RUN3_SLEEP + TIME_SAFE_MARGIN);
         long endTime = System.currentTimeMillis();
-        assertTime(startTime, endTime, RUN1_SLEEP + RUN4_SLEEP + 2 * TIME_SAFE_MARGIN);
+        assertTime(startTime, endTime, RUN1_SLEEP + RUN3_SLEEP + 2 * TIME_SAFE_MARGIN);
         assertEquals(10, map.size());
     }
 
@@ -106,23 +108,18 @@ class ThreadPoolTest {
         assertEquals(1, map.size());
     }
 
-    void run2() {
-        sleep(RUN2_SLEEP);
-        testThread.interrupt();
-    }
-
     void run1() {
         sleep(RUN1_SLEEP);
         map.put(new Object(), new Object());
     }
 
-    void run3() {
-        sleep(RUN3_SLEEP);
-        map.put(new Object(), new Object());
+    void run2() {
+        sleep(RUN2_SLEEP);
+        testThread.interrupt();
     }
 
-    void run4() {
-        sleep(RUN4_SLEEP);
+    void run3() {
+        sleep(RUN3_SLEEP);
         assertDoesNotThrow(() -> threadPool.invokeLater(this::run1));
     }
 
